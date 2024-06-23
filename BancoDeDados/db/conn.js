f3968a75ef095d4561365bd289de35930d3da71b
Diego <diegompaiva06@gmail.com>
2024-06-23 14:58:58 -0400

    Só mandando comentarios

--- BancoDeDados/db/conn.js
+++ BancoDeDados/db/conn.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   password: "vasco",
   database: "diego",
   port: 3306,
+  charset: "utf8mb4",
 });
 
 module.exports = pool;
--- BancoDeDados/index.js
+++ BancoDeDados/index.js
@@ -1,46 +1,57 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
+const express = require("express"); // importando express
+
+const exphbs = require("express-handlebars"); //importando express com handlebars
+
 //const mysql = require("mysql"); - Máquina pessoal
+
 //const mysql2 = require("mysql2") - Máquina do trabalho
-const pool = require("./db/conn");
-const path = require("path");
-const porta = 3000;
-const app = express();
 
-const basepath = path.join(__dirname, "views");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+const pool = require("./db/conn"); // importando conexão com o pool para conexão com banco de dados
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const path = require("path"); // importando core module para selecionar pastas
 
-app.engine("handlebars", exphbs.engine({ defaultLayout: false }));
-app.set("view engine", "handlebars");
+const porta = 3000; //expondo porta
+
+const app = express(); // colocando o express na váriavel app
+
+const basepath = path.join(__dirname, "views"); // comando para pegar a pasta views
+app.set("views", path.join(__dirname, "views")); // informa o Express onde encontrar a pasta views
+app.use(express.static(path.join(__dirname, "public"))); // comando para pegar a pasta de CSS
+
+app.use(express.urlencoded({ extended: true })); // middleware para pegar os dados URL
+app.use(express.json()); //middleware para pegar requisições JSON
+
+app.engine("handlebars", exphbs.engine({ defaultLayout: false })); // configura o motor de templates handlebars
+app.set("view engine", "handlebars"); // define handlebars como o padrão de visualização
 
 app.get("/app", (req, res) => {
-  res.sendFile(`${basepath}/app.html`);
+  // código que manda uma página para o usuario
+  res.sendFile(`${basepath}/app.html`); // arquivo onde está a página
 });
 
 app.post("/enviar-dados/livros", (req, res) => {
+  // código que pega dados que o usuario mandou
   const titulos = req.body.titulos;
   const paginas = req.body.paginas;
 
-  const sql = `INSERT INTO livros (??, ??) VALUES (?, ?)`;
+  const sql = `INSERT INTO livros (??, ??) VALUES (?, ?)`; // inserir o que o usuario mandou no banco de dados
 
   const values = ["titulos", "paginas", titulos, paginas];
 
   pool.query(sql, values, function (err) {
+    // esse query é pra transformar o código em SQL
     if (err) {
       console.log(`ERRO: ${err}`);
       return;
     }
 
-    res.redirect("/");
+    res.redirect("/"); // se tudo ocorrer corretamente ele redireciona para a pasta principal
   });
 });
 
 app.get("/livros", (req, res) => {
-  const sql = "SELECT * FROM livros";
+  // manda o usuario para a página /livros
+  const sql = "SELECT * FROM livros"; // comandos SQL
 
   pool.query(sql, function (err, dados) {
     if (err) {
@@ -48,14 +59,14 @@ app.get("/livros", (req, res) => {
       return;
     }
     const livros = dados;
-    //console.log(livros);
 
     res.render("livros", { livros });
   });
 });
 
 app.get("/dados/:id", (req, res) => {
-  const id = req.params.id;
+  // manda o usuario para a página /dados/:id e esse id é selecionado na hora que se clica em um livro
+  const id = req.params.id; // aqui ele pega o parametro de id vindo da página /dados/:id lá da pasta views
 
   const sql = `SELECT * FROM livros WHERE ?? = ?`;
 
@@ -133,5 +144,6 @@ app.post("/livros/remover/:id", (req, res) => {
 });
 
 app.listen(porta, () => {
+  // código para rodar a porta
   console.log(`Rodando na porta 3000`);
 });
